747e325ff96d35d83a16a949e19e092c7323208e
PowerBridge-ai <powerbridge.ai@gmail.com>
2025-03-28 02:36:15 +0000

    Revert to CommonJS for configuration files

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-export default () => {
-  return {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
-  }
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 } 
